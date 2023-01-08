ec441cb46e0ed7e853012ac1273fb407897d7a5f
Yana-Denisova <ladymacbeth@list.ru>
2023-01-08 12:33:16 +0300

    added delete project

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -2,15 +2,20 @@ from fastapi import APIRouter, Depends
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.user import current_superuser
 from app.core.db import get_async_session
-from app.crud.charity_project import create_charity_project, get_all, update_charity_project
+from app.crud.charity_project import create_charity_project, get_all, update_charity_project, delete_project
 from app.schemas.charity_project import CharityProjectCreate, CharityProjectUpdate
 from app.api.validators import check_name_duplicate, check_charity_project_exists, check_if_project_closed, check_sum
 
 router = APIRouter()
 
 
-@router.post('/', response_model_exclude_none=True,)
+@router.post(
+    '/',
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
+)
 async def create_new_charity_project(
     charity_project: CharityProjectCreate,
     session: AsyncSession = Depends(get_async_session),
@@ -35,6 +40,7 @@ async def get_all_charity_projects(
 @router.patch(
     '/{project_id}',
     response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
 )
 async def partially_update_charity_project(
     project_id: int,
@@ -55,3 +61,20 @@ async def partially_update_charity_project(
         project, obj_in, session
     )
     return updated_project
+
+
+@router.delete(
+    '/{project_id}',
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
+)
+async def delete_charity_project(
+    project_id: int,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Только для суперюзеров."""
+    project = await check_charity_project_exists(
+        project_id, session
+    )
+    project_deleted = await delete_project(project, session)
+    return project_deleted
--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str
     app_description: str
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    secret: str = 'secret'
 
     class Config:
         env_file = '.env'
